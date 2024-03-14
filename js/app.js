921e8c2beb021fe201408a50cfab487ec4dc0564
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-14 13:22:15 +0100

    ADD: commenti.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//scrivi un programma che stampi in console i numeri da 1 a 100
+
+//Aggiungi un controllo sul numero
+
+//Per i multipli di 3 stampare "Fizz" al posto del numero
+
+//Per i multipli di 5 stampare "Buzz" al posto del numero
+
+//Per i multipli che sono sia multipli di 3 che di 5 stampare "FizzBuzz"
+
+//In tutti gli altri casi stampare il numero stesso
+
+//Come faccio a sapere se un numero è divisibile per un altro?
