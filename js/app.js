5e5f51fdea3b2e7a6544afacf0b3312e4edd86fd
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-14 14:31:50 +0100

    ADD: programma per stampare in console numeri da 1 a 100. Aggiunto controllo sul numero.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 //scrivi un programma che stampi in console i numeri da 1 a 100
+//Come faccio a sapere se un numero è divisibile per un altro?
+//Utilizzando l'operatore modulo che ci dirà che se il resto dell'operazione è zero allora il numero è divisibile per un altro.
 
-//Aggiungi un controllo sul numero
+for (let i = 1; i <= 100; i++) {
 
-//Per i multipli di 3 stampare "Fizz" al posto del numero
+    //Aggiungi un controllo sul numero
+    //Per i multipli che sono sia multipli di 3 che di 5 stampare "FizzBuzz"
+
+if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+}
+   //Per i multipli di 3 stampare "Fizz" al posto del numero
+    else if (i % 3 === 0) {
+        console.log('Fizz');
+    }
 
 //Per i multipli di 5 stampare "Buzz" al posto del numero
 
-//Per i multipli che sono sia multipli di 3 che di 5 stampare "FizzBuzz"
+    else if (i % 5 === 0) {
+        console.log('Buzz');
+    }
 
-//In tutti gli altri casi stampare il numero stesso
+    else {
+        console.log('Nume:' + i);
+    }
 
-//Come faccio a sapere se un numero è divisibile per un altro?
+}
+
